df95c57472a548d03259fc92237e71939fc7c4f2
Marc Boanas <marcjohnboanas@gmail.com>
2020-10-09 09:54:52 +0100

    Initial Commit

---
+++ Save-A-Roo/Save_A_RooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_A_RooApp.swift
+//  Save-A-Roo
+//
+//  Created by Marc Boanas on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Save_A_RooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Save-A-RooTests/Save_A_RooTests.swift
@@ -0,0 +1,33 @@
+//
+//  Save_A_RooTests.swift
+//  Save-A-RooTests
+//
+//  Created by Marc Boanas on 09/10/2020.
+//
+
+import XCTest
+@testable import Save_A_Roo
+
+class Save_A_RooTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
